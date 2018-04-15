e738f2128bb3dbe9f275e405983f2937847377f6
Sudarshan Gaikaiwari <sudarshan@acm.org>
2018-04-14 20:12:30 -0500

    Defaults variables declarations to const, and functions to arrow functions

--- src/sudoku/sudoku.js
+++ src/sudoku/sudoku.js
@@ -21,18 +21,18 @@ let subregion = (rowTriples, colTriples) => {
   return result;
 };
 
-let digits = '123456789';
-let rows = 'ABCDEFGHI';
-let cols = digits;
-export let squares = cross(rows, digits);
-let subregion3x3 = subregion(['ABC', 'DEF', 'GHI'], ['123', '456', '789']);
+const digits = '123456789';
+const rows = 'ABCDEFGHI';
+const cols = digits;
+export const squares = cross(rows, digits);
+const subregion3x3 = subregion(['ABC', 'DEF', 'GHI'], ['123', '456', '789']);
 
-export let unitlist = []
+export const unitlist = []
   .concat(cols.split('').map(c => cross(rows, c)))
   .concat(rows.split('').map(r => cross(r, cols)))
   .concat(subregion3x3);
 
-export let units = new Map(
+export const units = new Map(
   squares.map(s => {
     let result = [];
     for (let u of unitlist) {
@@ -44,7 +44,7 @@ export let units = new Map(
   })
 );
 
-export let peers = new Map(squares.map(s => [s, new Set([].concat(...units.get(s)).filter(x => x != s))]));
+export const peers = new Map(squares.map(s => [s, new Set([].concat(...units.get(s)).filter(x => x != s))]));
 /**
  * Assigns the digits from the input grid to the squares.
  *
@@ -53,7 +53,7 @@ export let peers = new Map(squares.map(s => [s, new Set([].concat(...units.get(s
  * @return {Fales|Map} False if unable to assign digits to square else
  * a Map with the assignments
  */
-export function parseGrid(grid) {
+export const parseGrid = grid => {
   let values = new Map(squares.map(s => [s, digits]));
   for (let [s, d] of gridValues(grid).entries()) {
     if (digits.includes(d) && !assign(values, s, d)) {
@@ -61,7 +61,7 @@ export function parseGrid(grid) {
     }
   }
   return values;
-}
+};
 /**
  * Assigns digit 'd' to square 's'
  *
@@ -72,15 +72,15 @@ export function parseGrid(grid) {
  * @return {False|Map}: False if unable to assign digits to square else
  * a Map with the assignments
  */
-export function assign(values, s, d) {
-  let otherValues = values.get(s).replace(d, '');
-  let result = otherValues.split('').map(d2 => eliminate(values, s, d2));
+export const assign = (values, s, d) => {
+  const otherValues = values.get(s).replace(d, '');
+  const result = otherValues.split('').map(d2 => eliminate(values, s, d2));
   if (result.every(r => r != false)) {
     return values;
   } else {
     return false;
   }
-}
+};
 
 /**
  *
@@ -91,26 +91,26 @@ export function assign(values, s, d) {
  * @param {String} d : Digit
  * @return {False|Map}
  */
-export function eliminate(values, s, d) {
+export const eliminate = (values, s, d) => {
   // Eliminate d from values[s]; propagate when values or places <= 2.
   // Return values, except return False if a contradiction is detected.
   if (!values.get(s).includes(d)) {
     return values;
   }
   values.set(s, values.get(s).replace(d, ''));
-  let sDigits = values.get(s);
+  const sDigits = values.get(s);
   if (sDigits.length == 0) {
     return false;
   } else if (sDigits.length == 1) {
     // (1) If a square s is reduced to one value d2, then eliminate d2 from the peers.
-    let d2 = sDigits;
-    if (![...peers.get(s)].map(sq => eliminate(values, sq, d2)).every(r => r != false)) {
+    const d2 = sDigits;
+    if ([...peers.get(s)].map(sq => eliminate(values, sq, d2)).some(r => r == false)) {
       return false;
     }
   }
   // (2) If a unit u is reduced to only one place for a value d, then put it there.
   for (let u of units.get(s)) {
-    let dplaces = u.filter(sq => values.get(sq).includes(d));
+    const dplaces = u.filter(sq => values.get(sq).includes(d));
     if (dplaces.length == 0) {
       return false;
     } else if (dplaces.length == 1) {
@@ -120,7 +120,7 @@ export function eliminate(values, s, d) {
     }
   }
   return values;
-}
+};
 /**
  *
  *
@@ -129,8 +129,8 @@ export function eliminate(values, s, d) {
  * @return {Map}
  */
 export function gridValues(grid) {
-  let emptyChars = '0.';
-  let chars = grid.split('').filter(c => digits.includes(c) || emptyChars.includes(c));
+  const emptyChars = '0.';
+  const chars = grid.split('').filter(c => digits.includes(c) || emptyChars.includes(c));
   return new Map(zip(squares, chars));
 }
 
@@ -141,9 +141,9 @@ export function gridValues(grid) {
  * @param {any} values
  */
 export function display(values) {
-  let width = 1 + Math.max(...squares.map(s => values.get(s).length));
-  let dashes = '_'.repeat(width * 3);
-  let line = Array(3)
+  const width = 1 + Math.max(...squares.map(s => values.get(s).length));
+  const dashes = '_'.repeat(width * 3);
+  const line = Array(3)
     .fill(dashes)
     .join('+');
   for (let r of rows) {
@@ -169,8 +169,7 @@ export function display(values) {
  * @return {False|Map}
  */
 export function solve(grid) {
-  let values = parseGrid(grid);
-  return search(values);
+  return search(parseGrid(grid));
 }
 
 /**
@@ -186,19 +185,19 @@ export function search(values) {
   if (squares.every(s => values.get(s).length == 1)) {
     return values;
   }
-  let [, s] = squares
+  const [, s] = squares
     .map(s => {
-      let sDigits = values.get(s);
+      const sDigits = values.get(s);
       if (sDigits.length > 1) {
         return [sDigits.length, s];
       } else return null;
     })
-    .filter(s => {
-      return s != null;
+    .filter(mr => {
+      return mr != null;
     })
     .reduce((prev, curr) => (prev[0] < curr[0] ? prev : curr));
   for (let d of values.get(s)) {
-    let result = search(assign(new Map(values), s, d));
+    const result = search(assign(new Map(values), s, d));
     if (result) {
       return result;
     }
@@ -206,9 +205,9 @@ export function search(values) {
 }
 
 /* eslint-disable */
-let grid1 = '003020600900305001001806400008102900700000008006708200002609500800203009005010300';
-let grid2 = '4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......';
-let hard1 = '.....6....59.....82....8....45........3........6..3.54...325..6..................';
+const grid1 = '003020600900305001001806400008102900700000008006708200002609500800203009005010300';
+const grid2 = '4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......';
+const hard1 = '.....6....59.....82....8....45........3........6..3.54...325..6..................';
 /* eslint-enable */
 
 /**
@@ -218,10 +217,10 @@ let hard1 = '.....6....59.....82....8....45........3........6..3.54...325..6....
  * @return {Array<Time, Boolean>}
  */
 function timeSolve(grid) {
-  let start = process.hrtime();
-  let values = solve(grid);
-  let end = process.hrtime(start);
-  let t = end[0] * 1000 + end[1] / 1000000;
+  const start = process.hrtime();
+  const values = solve(grid);
+  const end = process.hrtime(start);
+  const t = end[0] * 1000 + end[1] / 1000000;
   return [t, solved(values)];
 }
 
@@ -237,8 +236,8 @@ function solved(values) {
     return false;
   }
   return unitlist.every(unit => {
-    let digitSet = new Set(digits.split(''));
-    let unitSet = new Set(unit.map(s => values.get(s)).sort());
+    const digitSet = new Set(digits.split(''));
+    const unitSet = new Set(unit.map(s => values.get(s)).sort());
     return digitSet.size == unitSet.size && [...digitSet].every(d => unitSet.has(d));
   });
 }
@@ -250,12 +249,12 @@ function solved(values) {
  * @param {string} [name='']
  **/
 function solveAll(grids, name = '') {
-  let [times, results] = zip(...grids.map(grid => timeSolve(grid)));
+  const [times, results] = zip(...grids.map(grid => timeSolve(grid)));
   if (results.some(r => r == false)) {
     console.log('Could not solve grid' + name);
     process.exit();
   }
-  let N = results.length;
+  const N = results.length;
   if (N < 1) {
     return;
   }
